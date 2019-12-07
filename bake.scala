f50c75f6248d1f3a2975aa1bd4bebbd9c856cd0b
Daniel Bertinshaw <bertinshaw.daniel@gmail.com>
2019-12-07 18:27:01 +1300

    Park scala, it has a nasty load time, and requires too much overhead for other people to run

--- bake.scala
+++ bake.scala
@@ -1,12 +1,52 @@
-#!/bin/sh
-exec scala "$0" "$@"
-!#
-import sys.process._
-
+// #!/bin/sh
+// exec scala "$0" "$@"
+// !#
+import scala.sys.process._
 import scala.language.postfixOps
+import java.io.{File, FileInputStream, InputStream}
 
-val mountData = "mount" !!
+case class Job(device:String, mountPoint:String) {
+  lazy val prettyName = mountPoint.split("/").last.replaceAll(" +", "_")
+  override def toString = s"""Job(device = "${device}", mountPoint = "${mountPoint}", prettyName = "${prettyName}")"""
 
-println(mountData)
+  val volumeListFile = new File(prettyName + ".flist")
+  val clamFile = new File(prettyName + ".clam")
 
-println("foo")
+  lazy val totalFiles = genFileList
+
+  def genFileList = {
+    def count(in:InputStream):Int = {
+      var ch = ' '
+      var count = 0
+      do {
+        ch = in.read()
+        if (ch == '\n') {
+          val ch2 = in.read()
+          if (ch2 == '/') count = count + 1
+          if (ch2 < 0) return count
+        }
+      }
+      while (ch > 0) ;
+      return count
+    }
+    // Seq("find", mountPoint, "-type", "f") #> volumeListFile !
+    val t = count(new FileInputStream(volumeListFile))
+    println(t)
+    // proc.!!.trim.toInt
+    12
+  }
+
+// sfill
+  // start clam with future
+}
+
+val jobs = {Process("mount") !!}
+            .split("\n")
+            .toList
+            .filter(_.matches("/dev/sd[^a][0-9]+.*"))
+            .map(_.split(" +"))
+            .map(line => Job(line(0), line(2)))
+
+jobs.foreach(_.totalFiles)
+
+// println(jobs)
